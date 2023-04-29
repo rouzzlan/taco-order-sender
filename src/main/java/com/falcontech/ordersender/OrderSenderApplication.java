1b4671a93bae8223e76021eaf273011c4b575a3f
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2023-04-29 21:13:57 +0200

    initial commit

---
+++ src/main/java/com/falcontech/ordersender/OrderSenderApplication.java
@@ -0,0 +1,13 @@
+package com.falcontech.ordersender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderSenderApplication.class, args);
+    }
+
+}
